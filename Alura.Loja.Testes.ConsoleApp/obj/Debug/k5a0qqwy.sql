e9c530e57084e5706b2b37cc12349d763e2d876a
andreevannascimento <evan.infor@gmail.com>
2019-07-17 12:10:46 -0300

    first commit

---
+++ Alura.Loja.Testes.ConsoleApp/obj/Debug/k5a0qqwy.sql
@@ -0,0 +1,13 @@
+﻿EXEC sp_rename N'Produtos.Preco', N'PrecoUnitario', N'COLUMN';
+
+GO
+
+ALTER TABLE [Produtos] ADD [Unidade] nvarchar(max);
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20170531184057_Unidade', N'1.1.2');
+
+GO
+
